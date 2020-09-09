1fdc6df5aba1db3e807343b1f4dc4abd617b0a80
Manourez <manourez@hotmail.fr>
2020-09-09 13:28:11 +0000

    update

---
+++ precache-manifest.65c846e7ab95db6052a2b95a25fbb9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac35735b821a1ac6088ba1f89c16442d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15480ff93bbc0f663f35",
+    "url": "/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15480ff93bbc0f663f35",
+    "url": "/static/js/main.8f7cf373.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
